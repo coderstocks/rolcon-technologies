fbe9053145fb6e7edafd3234c6ecf02461820e85
Viral Ads Media <info.viraladsmedia@gmail.com>
2024-04-12 11:52:40 +0530

    change

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+AOS.init({
+    duration: 1200,
+  })
+
+  $('.product-card').slick({
+    dots: false,
+    infinite: true,
+    centerMode: true,
+    arrows: false,
+    speed: 150,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
