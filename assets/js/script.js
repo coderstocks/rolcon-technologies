6feabd087dc18f4f1ab32446bbbc97c0601b50b5
Viral Ads Media <info.viraladsmedia@gmail.com>
2024-05-10 15:41:02 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,3 +40,29 @@ AOS.init({
       // instead of a settings object
     ]
   });
+
+  function initSwiper() {
+    document.querySelectorAll('.swiper').forEach(function(swiper) {
+      let config = JSON.parse(swiper.querySelector('.swiper-config').innerHTML.trim());
+      new Swiper(swiper, config);
+    });
+  }
+  window.addEventListener('load', initSwiper);
+
+  /**
+   * Correct scrolling position upon page load for URLs containing hash links.
+   */
+  window.addEventListener('load', function(e) {
+    if (window.location.hash) {
+      if (document.querySelector(window.location.hash)) {
+        setTimeout(() => {
+          let section = document.querySelector(window.location.hash);
+          let scrollMarginTop = getComputedStyle(section).scrollMarginTop;
+          window.scrollTo({
+            top: section.offsetTop - parseInt(scrollMarginTop),
+            behavior: 'smooth'
+          });
+        }, 100);
+      }
+    }
+  });
